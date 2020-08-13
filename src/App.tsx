648ba408798db05f959c86bd85c9aa26a3fec219
Kamil Sikora <sikorakamil26@gmail.com>
2020-08-13 12:44:41 +0200

    finishing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './theme/GlobalStyle';
+import theme from './theme/mainTheme';
 
-function App() {
+const App: React.FunctionComponent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <>Temp</>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/theme/mainTheme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  fontSize: {
+    s: '1.2rem',
+    m: '2.1rem',
+    l: '2.4rem',
+  },
+};
+
+export default theme;
