688d1c900d86903ba859fef3509366a321a212af
Kamil Sikora <sikorakamil26@gmail.com>
2020-08-16 18:42:02 +0200

    setup types and colors

---
+++ src/pokemonTypes.js
@@ -0,0 +1,25 @@
+export const types = {
+  bug: {
+    iconSmall: './assets/types/small/bug.svg',
+    iconbig: './assets/types/big/bug.svg',
+  },
+  dark: 'hello',
+  dragon: 'hello',
+  electric: 'hello',
+  fairy: 'hello',
+  fight: 'hello',
+  fire: 'hello',
+  flying: 'hello',
+  ghost: 'hello',
+  grass: 'hello',
+  ground: 'hello',
+  ice: 'hello',
+  normal: 'hello',
+  poison: 'hello',
+  psychic: 'hello',
+  rock: 'hello',
+  steel: 'hello',
+  water: 'hello',
+};
+
+export default types;
